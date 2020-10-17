a6bd3091993616131cfbbc243051d680c58d6963
Wolfo-Gaming <37922940+Wolfo-Gaming@users.noreply.github.com>
2020-10-17 15:47:48 +0200

    Update node-installer.sh

--- node-installer.sh
+++ node-installer.sh
@@ -1,8 +1,7 @@
 wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
 tar -xf node_exporter-0.18.1.linux-amd64.tar.gz
 mv node_exporter-0.18.1.linux-amd64 node_exporter
-cd /etc/systemd/system/
-cat <<EOT >> service.tmp
+cat <<EOT >> /etc/systemd/system/node_exporter.service
 [Unit]
 Description=Node Exporter
 Wants=network-online.target
